70cfa228fb73f0deeb37d316cdd294a2c55ff9e9
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2021-05-08 21:31:03 -0400

    fixed constants.py for biolink

--- trapi_model/constants.py
+++ trapi_model/constants.py
@@ -1,21 +1,21 @@
 import logging
 
 # Biolink Entities
-BIOLINK_GENE = 'gene'
-BIOLINK_DRUG = 'drug'
-BIOLINK_DISEASE = 'disease'
-BIOLINK_PHENOTYPIC_FEATURE = 'phenotypic feature'
+BIOLINK_GENE = 'biolink:Gene'
+BIOLINK_DRUG = 'biolink:Drug'
+BIOLINK_DISEASE = 'biolink:Disease'
+BIOLINK_PHENOTYPIC_FEATURE = 'biolink:PhenotypicFeature'
 
 # Edge Property Constants
-BIOLINK_CONTRIBUTION = 'has evidence'
-BIOLINK_PROBABILITY = 'has confidence level'
+BIOLINK_CONTRIBUTION = 'biolink:has_evidence'
+BIOLINK_PROBABILITY = 'biolink:has_confidence_level'
 
 # Biolink Predicate/Association/Slot Constants
-BIOLINK_GENE_TO_DISEASE_PREDICATE = 'gene associated with condition'
-BIOLINK_CHEMICAL_TO_DISEASE_OR_PHENOTYPIC_FEATURE_PREDICATE = 'treats'
-BIOLINK_CHEMICAL_TO_GENE_PREDICATE = 'interacts_with'
-BIOLINK_GENE_TO_CHEMICAL_PREDICATE = 'interacts_with'
-BIOLINK_DISEASE_TO_PHENOTYPIC_FEATURE_PREDICATE = 'has_phenotype'
+BIOLINK_GENE_TO_DISEASE_PREDICATE = 'biolink:gene_associated_with_condition'
+BIOLINK_CHEMICAL_TO_DISEASE_OR_PHENOTYPIC_FEATURE_PREDICATE = 'biolink:treats'
+BIOLINK_CHEMICAL_TO_GENE_PREDICATE = 'biolink:interacts_with'
+BIOLINK_GENE_TO_CHEMICAL_PREDICATE = 'biolink:interacts_with'
+BIOLINK_DISEASE_TO_PHENOTYPIC_FEATURE_PREDICATE = 'biolink:has_phenotype'
 
 # Maps
 SUBJECT_TO_OBJECT_PREDICATE_MAP = {
