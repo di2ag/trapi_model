5ee2e2b82c57fa452873b37ae7f44340be37eece
veenhouse <luke.veenhuis@gmail.com>
2021-06-15 02:02:23 -0500

    fixed import issue

--- processing_and_validation/semantic_processor_exceptions.py
+++ processing_and_validation/semantic_processor_exceptions.py
@@ -1,7 +1,7 @@
 from trapi_model.biolink import BiolinkEntity
 
 class UnsupportedCategoryAncestors(Exception):
-    def __init__(self, categories:list[BiolinkEntity], message:str='Unsupported Category Ancestor') -> None:
+    def __init__(self, categories:list, message:str='Unsupported Category Ancestor') -> None:
         self.message = message
         self.categories = [passed_name for category.passed_name in categories]
 
@@ -11,7 +11,7 @@ class UnsupportedCategoryAncestors(Exception):
         return '{}: {}'.format(self.message, self.categories)
 
 class UnsupportedPredicateAncestor(Exception):
-    def __init__(self, predicates:list[BiolinkEntity], message:str = 'Unsupported Predicate Ancestor') -> None:
+    def __init__(self, predicates:list, message:str = 'Unsupported Predicate Ancestor') -> None:
         self.message = message
         self.predicate = [passed_name for predicate.passed_name in predicates]
         super().__init__(message)
@@ -20,7 +20,7 @@ class UnsupportedPredicateAncestor(Exception):
         return '{}: {}'.format(self.message, self.predicate)
 
 class IndeterminableCategoryDescendent(Exception):
-    def __init__(self, categories:list[BiolinkEntity], message:str = 'Indeterminable Category Descendent') -> None:
+    def __init__(self, categories:list, message:str = 'Indeterminable Category Descendent') -> None:
         self.message = message
         self.categories = [passed_name for categories.passed_name in categories]
         super().__init__(message)
