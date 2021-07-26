9ac29f2bc0e90d829a3030f82c3b752ae596011b
veenhouse <luke.veenhuis@gmail.com>
2021-07-26 17:32:07 -0500

    updated semantic operations

--- trapi_model/processing_and_validation/semantic_processor_exceptions.py
+++ trapi_model/processing_and_validation/semantic_processor_exceptions.py
@@ -1,23 +1,23 @@
 class UnsupportedCategoryAncestors(Exception):
-    def __init__(self, categories:list, message:str='Unsupported Category Ancestor') -> None:
+    def __init__(self, category:str, message:str='Unsupported Category Ancestor') -> None:
         self.message = message
-        self.categories = [category.passed_name for category in categories]
-
+        #self.categories = [category.passed_name for category in categories]
+        self.category = category
         super().__init__(message)
 
     def __str__(self) -> str:
-        return '{}: {}'.format(self.message, self.categories)
+        return '{}: {}'.format(self.message, self.category)
 
 class UnsupportedPredicateAncestor(Exception):
-    def __init__(self, predicates:list, message:str = 'Unsupported Predicate Ancestor') -> None:
+    def __init__(self, predicate:str, message:str = 'Unsupported Predicate Ancestor') -> None:
         self.message = message
-        self.predicate = [predicate.passed_name for predicate in predicates]
+        self.predicate = predicate
         super().__init__(message)
 
     def __str__(self) -> str:
         return '{}: {}'.format(self.message, self.predicate)
 
-class IndeterminableCategoryDescendent(Exception):
+class IndeterminableWildcardDescendent(Exception):
     def __init__(self, categories:list, message:str = 'Indeterminable Category Descendent') -> None:
         self.message = message
         self.categories = [category.passed_name for category in categories]
