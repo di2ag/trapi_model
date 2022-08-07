fc3eca87f3425ee744c83ee4d0b91c7a43a11ba3
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2022-08-07 10:23:49 -0400

    bandaid to do query_id in node bindings for curie descendant mapping for trapi 1.3

--- trapi_model/results.py
+++ trapi_model/results.py
@@ -21,12 +21,13 @@ class Result(TrapiBaseClass):
         self.edge_bindings = defaultdict(list)
         super().__init__(trapi_version, biolink_version)
 
-    def add_node_binding(self, qg_id, kg_id):
+    def add_node_binding(self, qg_id, kg_id, conflate_term = None):
         self.node_bindings[qg_id].append(
                 Binding(
                     self.trapi_version,
                     self.biolink_version,
-                    kg_id
+                    kg_id,
+                    conflate_term = conflate_term
                     )
                 )
     
@@ -95,9 +96,13 @@ class Results(TrapiBaseClass):
 
     def add_result(self, node_bindings, edge_bindings):
         result = Result(self.trapi_version, self.biolink_version)
+        conflate_term = None
+        if 'query_id' in node_bindings:
+            conflate_term = node_bindings['query_id']
         for qg_id, kg_ids in node_bindings.items():
-            for kg_id in kg_ids:
-                result.add_node_binding(qg_id, kg_id)
+            if qg_id != 'query_id':
+                for kg_id in kg_ids:
+                    result.add_node_binding(qg_id, kg_id, conflate_term = conflate_term)
         for qg_id, kg_ids in edge_bindings.items():
             for kg_id in kg_ids:
                 result.add_edge_binding(qg_id, kg_id)
@@ -120,12 +125,17 @@ class Results(TrapiBaseClass):
         return new_results
 
 class Binding(TrapiBaseClass):
-    def __init__(self, trapi_version, biolink_version, kg_id=None):
+    def __init__(self, trapi_version, biolink_version, kg_id=None, conflate_term = None):
         self.id = kg_id
+        self.conflate_term = conflate_term
         super().__init__(trapi_version, biolink_version)
     
     def to_dict(self):
-        return {"id": self.id}
+        if self.conflate_term is None:
+            return {"id": self.id}
+        else:
+            return {"id":self.id,
+                    "query_id":self.conflate_term}
 
     @staticmethod
     def load(trapi_version, biolink_version, binding_info):
